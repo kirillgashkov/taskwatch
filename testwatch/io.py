ad31ebb43d5a862e868c558cc55e481d239b9e71
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-25 22:14:18 +0500

    Add 'io' module (#1)
    * Add 'readline' function
    
    * Add 'ask' function
    
    * Add 'confirm' function
    
    * Add 'info' function
    
    * Add 'error' function

---
+++ testwatch/io.py
@@ -0,0 +1,37 @@
+#
+# Input
+#
+
+
+def readline():
+    s = input('>>> ').strip()
+    return s or readline()
+
+
+def ask(prompt):
+    if prompt:
+        print(prompt)
+    s = readline().strip()
+    return s
+
+
+def confirm(prompt):
+    s = ask(prompt).lower()
+    if s in {'y', 'yes'}:
+        return True
+    if s in {'n', 'no'}:
+        return False
+    return confirm(prompt)
+
+
+#
+# Output
+#
+
+
+def info(s):
+    print(s)
+
+
+def error(s):
+    print(s)
