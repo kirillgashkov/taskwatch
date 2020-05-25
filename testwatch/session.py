52397c089f6729d37b02e88f80bde7abe18908f4
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-26 01:05:39 +0500

    Add 'write' function

--- testwatch/session.py
+++ testwatch/session.py
@@ -7,3 +7,18 @@ def start():
 
 def end():
     store.add_entry(watch.current_time(), 'end', '')
+
+
+def write(s):
+    if s.startswith('*') or s.endswith('*'):
+        _amend(s.strip('*').strip())
+    else:
+        _record(s)
+
+
+def _record(s):
+    store.add_entry(watch.current_time(), 'record', s)
+
+
+def _amend(s):
+    store.add_entry(watch.current_time(), 'amend', s)
