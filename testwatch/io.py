b1bc8a275cba3b8d5b8df2835a0c38eae527bada
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-25 21:58:32 +0500

    Add 'confirm' function

--- testwatch/io.py
+++ testwatch/io.py
@@ -13,3 +13,12 @@ def ask(prompt):
         print(prompt)
     s = readline().strip()
     return s
+
+
+def confirm(prompt):
+    s = ask(prompt).lower()
+    if s in {'y', 'yes'}:
+        return True
+    if s in {'n', 'no'}:
+        return False
+    return confirm(prompt)
