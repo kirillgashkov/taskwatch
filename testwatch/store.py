57e0a81214a7debc193ef7449fbc125b154ad6d1
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-26 13:54:07 +0500

    Clean up 'store' module (#9)
    * Add file sections
    
    * Add '_set_last_entry' utility function
    
    * Add '_make_line_from_entry' utility function
    
    * Add '_get_first_and_last_lines' utility function
    
    * Add '_make_session_file_complete' utility function
    
    * Stop outer scope name shadowing
    
    * Remove '_handle_session_file' utility function

--- testwatch/store.py
+++ testwatch/store.py
@@ -6,6 +6,11 @@ from testwatch import io
 SESSION_FILE = '.testwatch_session'
 
 
+#
+# Entry
+#
+
+
 class Entry:
     def __init__(self, timestamp, entry_type, entry_content):
         self.timestamp = timestamp
@@ -18,37 +23,54 @@ def _make_entry_from_line(s):
     return Entry(timestamp, entry_type, entry_content)
 
 
-_last_entry = Entry(-1, '', '')
+def _make_line_from_entry(entry):
+    return f'{entry.timestamp}\t{entry.type}\t{entry.content}'
+
+
+#
+# Init
+#
 
 
 def init():
     if os.path.exists(SESSION_FILE):
         io.info('Session file is detected.')
-        _handle_session_file()
+
+        first_line, last_line = _get_first_and_last_lines(SESSION_FILE)
+
+        if not first_line:
+            return
+
+        first_entry_ = _make_entry_from_line(first_line)
+        last_entry_ = _make_entry_from_line(last_line)
+
+        if last_entry_.type == 'end':
+            _make_session_file_complete(first_entry_)
+            io.info('Complete session file is created.')
+        else:
+            _set_last_entry(last_entry_)
+            io.info('Last session file is loaded.')
 
 
-def _handle_session_file():
-    with open(SESSION_FILE) as f:
+def _get_first_and_last_lines(file):
+    with open(file) as f:
         first_line = f.readline().rstrip('\n')
         last_line = first_line
 
         for line in f:
             last_line = line
 
-    if not first_line:
-        return
+    return first_line, last_line
 
 
-    first_entry = _make_entry_from_line(first_line)
-    last_entry = _make_entry_from_line(last_line)
+def _make_session_file_complete(first_entry):
+    complete_session_file = f'{SESSION_FILE}_{first_entry.timestamp}'
+    os.rename(SESSION_FILE, complete_session_file)
 
-    if last_entry.type == 'end':
-        os.rename(SESSION_FILE, f'{SESSION_FILE}_{first_entry.timestamp}')
-        io.info('Complete session file is created.')
-    else:
-        global _last_entry
-        _last_entry = last_entry
-        io.info('Last session file is loaded.')
+
+#
+# Add entry
+#
 
 
 def add_entry(timestamp, entry_type, entry_content):
@@ -59,12 +81,27 @@ def add_entry(timestamp, entry_type, entry_content):
         )
         return
 
-    with open(SESSION_FILE, 'a') as f:
-        f.write(f'{timestamp}\t{entry_type}\t{entry_content}\n')
+    entry = Entry(timestamp, entry_type, entry_content)
 
-    global _last_entry
-    _last_entry = Entry(timestamp, entry_type, entry_content)
+    with open(SESSION_FILE, 'a') as f:
+        line = _make_line_from_entry(entry)
+        f.write(f'{line}\n')
+
+    _set_last_entry(entry)
+
+
+#
+# Last entry
+#
+
+
+_last_entry = Entry(-1, '', '')
 
 
 def last_entry():
     return _last_entry
+
+
+def _set_last_entry(entry):
+    global _last_entry
+    _last_entry = entry
