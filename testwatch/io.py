776a0a15c64ffb096278c96ae4cabf2223e276de
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-05-26 20:02:41 +0500

    Add 'format_time' function

--- testwatch/io.py
+++ testwatch/io.py
@@ -45,6 +45,17 @@ def error(s):
 #
 
 
+def format_time(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+
+    h_str = f'{h}h ' if h else ''
+    m_str = f'{m}m ' if m else ''
+    s_str = f'{s}s'
+
+    return h_str + m_str + s_str
+
+
 def format_date(unix):
     fmt = '%Y-%m-%d %H:%M:%S'
     return datetime.datetime.utcfromtimestamp(unix).strftime(fmt)
